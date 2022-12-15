4b4ea777fd3b5946a98f6573b95bd3f4dba77acc
Nidasakinaa <114220455+Nidasakinaa@users.noreply.github.com>
2022-12-15 16:36:27 +0700

    Add files via upload

---
+++ Object.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipe data object</title>
+</head>
+<body>
+    <h2>Tipe data objek</h2>
+    <?php
+    class test
+    {
+        var$str
+    }
+</body>
+</html>
